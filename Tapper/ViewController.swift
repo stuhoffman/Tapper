037cb65c2e8d89417dcbec4f3c0b6b17f275d416
Stuart Hoffman <stu@hoffmanswebsite.com>
2016-02-17 16:50:14 -0800

    Wired up the buttons with IBOutlet and IBAction

--- Tapper/ViewController.swift
+++ Tapper/ViewController.swift
@@ -10,6 +10,26 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var logoImage: UIImageView!
+    @IBOutlet weak var howManyTapsTxt: UITextField!
+    @IBOutlet weak var playButton: UIButton!
+    @IBOutlet var coinButton: UIButton!
+    
+    @IBOutlet weak var tapButton: UIButton!
+    @IBOutlet weak var  tapsLabel: UILabel!
+    
+    @IBAction func onPlayButtonPressed(sender: UIButton!)
+    {
+        logoImage.hidden = true
+        playButton.hidden = true
+        howManyTapsTxt.hidden = true
+        
+        //tapButton.hidden = false
+        tapsLabel.hidden = false
+        coinButton.hidden = false
+    }
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
